1736f8377ea9c0542eb0e31de9f163781a621ff8
Pedro Dava <josepedrodava@gmail.com>
2020-10-19 17:12:58 +0200

    Update login.dart
    - Color  Changes
    - Added  Google Sign in Functionality
    - Get  signed in  user  Profile
    - Rounded buttons  (" IGOR SAMBO  contribution")

--- lib/views/login.dart
+++ lib/views/login.dart
@@ -1,5 +1,13 @@
+import 'dart:async'; // New
+import 'dart:convert' show json; // New
+import "package:http/http.dart" as http; // New
+import 'package:google_sign_in/google_sign_in.dart';
+// New
+
 import 'package:flutter/material.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
+
+// New
 
 class Login extends StatefulWidget {
   @override
@@ -7,14 +15,19 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  GoogleSignIn _googleSignIn = GoogleSignIn(
+    scopes: <String>['email', 'profile'],
+  );
   bool _isVisible = true;
-  var buttonShape = RoundedRectangleBorder(borderRadius: BorderRadius.circular(80));
+  String name = "", email = "", profilePic = "", gender = "";
+  String photoUrl =
+      "https://firebasestorage.googleapis.com/v0/b/myplayground-2d31d.appspot.com/o/undraw_flutter_dev_wvqj%20(1).png?alt=media&token=b00d0917-26c1-43fb-b1e3-ad7d709f96ce";
 
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromRGBO(87, 60, 139, 1),
+      backgroundColor: Color.fromRGBO(249, 168, 38, 1),
       body: Column(
+        //Centering  widgets
         crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
@@ -23,21 +36,29 @@ class _LoginState extends State<Login> {
               radius: 80.0,
               backgroundColor: Colors.white,
               child: CircleAvatar(
-                radius: 70.0,
-                backgroundImage: NetworkImage(
-                    "https://lh3.googleusercontent.com/aJ9dKgc96wvdv50Aedz_G2g5WlSIOznnW9dTyrqxrCTzdgVBEI_F8z4MnPSkdQgc7HNp=s180-rw"),
+                radius: 75.0,
+                backgroundImage: NetworkImage(photoUrl),
                 backgroundColor: Colors.white,
               ),
             ),
           ),
           SizedBox(height: 60),
-          Visibility(
+          /*    Visibility(
             child: signInInformation(),
             visible: !_isVisible,
+          ),*/
+          Visibility(
+            child: signInButtons(),
+            visible: _isVisible,
           ),
           Visibility(
-            child: signInMethod(),
-            visible: _isVisible,
+            child: profileDetails(),
+            visible: !_isVisible,
+          ),
+          SizedBox(height: 60),
+          Visibility(
+            child: signOutButton(),
+            visible: !_isVisible,
           ),
         ],
       ),
@@ -83,9 +104,9 @@ class _LoginState extends State<Login> {
           width: 150,
           child: RaisedButton(
             elevation: 2,
-            color: Color.fromRGBO(87, 60, 139, 0.7),
+            color: Color.fromRGBO(249, 168, 38, 0.7),
             textColor: Colors.white,
-            splashColor: Color.fromRGBO(87, 60, 139, 0.5),
+            splashColor: Color.fromRGBO(249, 168, 38, 0.5),
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(18.0),
               side: BorderSide(color: Colors.white),
@@ -105,13 +126,48 @@ class _LoginState extends State<Login> {
     );
   }
 
-  Widget signInMethod() {
+  Widget signOutButton() {
+    return Container(
+      width: 150,
+      child: FlatButton(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(18.0),
+        ),
+        child: Text('Sign Out'),
+        textColor: Colors.black,
+        color: Colors.white,
+        onPressed: () {
+          _googleSignIn
+              .signOut()
+              .then((value) => print("Out"))
+              .catchError((onError) => print(onError));
+
+          setState(() {
+            photoUrl =
+                "https://firebasestorage.googleapis.com/v0/b/myplayground-2d31d.appspot.com/o/undraw_flutter_dev_wvqj%20(1).png?alt=media&token=b00d0917-26c1-43fb-b1e3-ad7d709f96ce";
+            _isVisible = !_isVisible;
+          });
+        },
+      ),
+    );
+  }
+
+  Widget signInButtons() {
     return Column(
       children: [
         SignInButton(
           Buttons.Google,
           text: "Sign in  with  Google",
-          onPressed: () {},shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+          onPressed: () {
+            _googleSignIn.signIn().then((response) {
+              setState(() {
+                name = response.displayName;
+                email = response.email;
+                photoUrl = response.photoUrl;
+                _isVisible = false;
+              });
+            }).catchError((onError) => print(onError));
+          },
         ),
         SizedBox(height: 20),
         Text(
@@ -123,40 +179,53 @@ class _LoginState extends State<Login> {
           width: 200,
           child: Row(
             children: [
-
-
               SignInButtonBuilder(
-                text: 'with Email',
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(18.0),
+                ),
+                text: 'With Email',
                 icon: Icons.email,
                 iconColor: Colors.blueGrey[700],
                 mini: true,
-                onPressed: () {
-                  setState(() {
-                    _isVisible = !_isVisible;
-                  });
-                },
-                backgroundColor: Colors.white,shape: buttonShape
-
+                onPressed: null,
+                backgroundColor: Colors.white,
               ),
-              SignInButton(
-                Buttons.Facebook,
-                mini: true,
-                onPressed: () {},shape: buttonShape,
-              ),
+              SignInButton(Buttons.Facebook,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(18.0),
+                  ),
+                  mini: true,
+                  onPressed: null),
               SignInButton(
                 Buttons.Apple,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(18.0),
+                ),
                 mini: true,
-                onPressed: () {},shape: buttonShape,
+                onPressed: null,
               ),
               SignInButton(
                 Buttons.GitHub,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(18.0),
+                ),
                 mini: true,
-                onPressed: () {},shape: buttonShape,
+                onPressed: null,
               )
             ],
           ),
         ),
       ],
+    );
+  }
+
+  Widget profileDetails() {
+    return Container(
+      width: 300,
+      child: Column(children: [
+        Text(name, style: TextStyle(color: Colors.white, fontSize: 20)),
+        Text(email, style: TextStyle(color: Colors.white, fontSize: 18)),
+      ]),
     );
   }
 }
